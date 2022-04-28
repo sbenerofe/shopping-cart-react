bc169f316ecff4e3dd257e57fa2d0cac7640a284
Steven Benerofe <sbenerofe@gmail.com>
2022-04-28 15:59:00 -0700

    update product list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import pokeMART from '/home/akmal-izuddin/Desktop/the-odin-project/pokemart-react/src/assets/pokeMART.jpg';
+import pokeMART from '../assets/pokeMART.jpg';
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
     <div className='Home'>
-      <img src={ pokeMART } alt='POKEMART ICON' />
-      <div className='greetings' >
-      <p>Welcome to the Poké Mart!</p>
-      <p>May I help you?</p>
-      <button onClick={ () => { navigate( '/pokemart-react/shop' ) } } >BUY</button>
+      <img src={pokeMART} alt='POKEMART ICON' />
+      <div className='greetings'>
+        <p>Welcome to the Poké Mart!</p>
+        <p>May I help you?</p>
+        <button
+          onClick={() => {
+            navigate('/pokemart-react/shop');
+          }}
+        >
+          BUY
+        </button>
       </div>
     </div>
   );
--- src/components/data.js
+++ src/components/data.js
@@ -1,69 +1,47 @@
-import pokeBallIcon from '/home/akmal-izuddin/Desktop/the-odin-project/pokemart-react/src/assets/item-images/pokeBall.webp';
-import greatBallIcon from '/home/akmal-izuddin/Desktop/the-odin-project/pokemart-react/src/assets/item-images/greatBall.png';
-import ultraBallIcon from '/home/akmal-izuddin/Desktop/the-odin-project/pokemart-react/src/assets/item-images/ultraBall.webp';
-import maxPotionIcon from '/home/akmal-izuddin/Desktop/the-odin-project/pokemart-react/src/assets/item-images/maxPotion.png';
-import reviveIcon from '/home/akmal-izuddin/Desktop/the-odin-project/pokemart-react/src/assets/item-images/revive.webp';
-import fullHealIcon from '/home/akmal-izuddin/Desktop/the-odin-project/pokemart-react/src/assets/item-images/fullHeal.webp';
-import escapeRopeIcon from '/home/akmal-izuddin/Desktop/the-odin-project/pokemart-react/src/assets/item-images/escapeRope.webp';
-import superRepelIcon from '/home/akmal-izuddin/Desktop/the-odin-project/pokemart-react/src/assets/item-images/superRepel.webp';
+import integrity from '../assets/item-images/integrity.jpg';
+import belonging from '../assets/item-images/belonging.jpg';
+import curiosity from '../assets/item-images/curiosity.jpg';
+import joy from '../assets/item-images/joy.png';
+import respect from '../assets/item-images/respect.jpg';
 
-const data = { catalog: [
-  {
-    id: '01',
-    icon: pokeBallIcon,
-    name: 'POKE BALL',
-    price: 200,
-    quantity: 0,
-  },
-  {
-    id: '02',
-    icon: greatBallIcon,
-    name: 'GREAT BALL',
-    price: 600,
-    quantity: 0,
-  },
-  {
-    id: '03',
-    icon: ultraBallIcon,
-    name: 'ULTRA BALL',
-    price: 1200,
-    quantity: 0,
-  },
-  {
-    id: '04',
-    icon: maxPotionIcon,
-    name: 'MAX POTION',
-    price: 2500,
-    quantity: 0,
-  },
-  {
-    id: '05',
-    icon: reviveIcon,
-    name: 'REVIVE',
-    price: 1500,
-    quantity: 0,
-  },
-  {
-    id: '06',
-    icon: fullHealIcon,
-    name: 'FULL HEAL',
-    price: 600,
-    quantity: 0,
-  },
-  {
-    id: '07',
-    icon: escapeRopeIcon,
-    name: 'ESCAPE ROPE',
-    price: 550,
-    quantity: 0,
-  },
-  {
-    id: '08',
-    icon: superRepelIcon,
-    name: 'SUPER REPEL',
-    price: 2500,
-    quantity: 0,
-  },
-]};
+const data = {
+  catalog: [
+    {
+      id: '01',
+      icon: integrity,
+      name: 'INTEGRITY',
+      price: 200,
+      quantity: 0,
+    },
+    {
+      id: '02',
+      icon: belonging,
+      name: 'BELONGING',
+      price: 600,
+      quantity: 0,
+    },
+    {
+      id: '03',
+      icon: curiosity,
+      name: 'CURIOSITY',
+      price: 1200,
+      quantity: 0,
+    },
+    {
+      id: '04',
+      icon: joy,
+      name: 'JOY',
+      price: 2500,
+      quantity: 0,
+    },
+    {
+      id: '05',
+      icon: respect,
+      name: 'RESPECT',
+      price: 1500,
+      quantity: 0,
+    },
+  ],
+};
 
 export { data };
